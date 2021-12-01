e0bca42019f817f0d028acdcf590c39cacbc24ec
Christophe Cutullic <christophe.cutullic@b-com.com>
2021-12-01 12:01:26 +0100

    Remove all Service projects from Sample-Mapping

--- Mapping/SolARService_Mapping_Multi/docker/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t artwin/solar/services/mapping-multi-service -f SolARServiceMappingMulti.dockerfile ..
--- Mapping/SolARService_Mapping_Multi/tests/SolARServiceTest_Mapping_Multi_Producer/docker/build_producer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build -t artwin/solar/services/mapping-multi-producer -f SolARServiceMappingMultiProducer.dockerfile ..
-

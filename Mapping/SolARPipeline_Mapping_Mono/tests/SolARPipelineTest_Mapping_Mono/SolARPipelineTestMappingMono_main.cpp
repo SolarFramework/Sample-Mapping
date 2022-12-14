70cf39f575693269cc08d47668a2b073dafea337
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2022-12-14 09:17:48 +0100

    Add inline comment for fixedPose boolean literal parameter

--- Mapping/SolARPipeline_Mapping_Mono/tests/SolARPipelineTest_Mapping_Mono/SolARPipelineTestMappingMono_main.cpp
+++ Mapping/SolARPipeline_Mapping_Mono/tests/SolARPipelineTest_Mapping_Mono/SolARPipelineTestMappingMono_main.cpp
@@ -130,7 +130,7 @@ auto fnClientProducer = []() {
             // send to mapping
             Transform3Df updateT_M_W;
             MappingStatus status;
-            gMappingPipelineMono->mappingProcessRequest({image}, {pose}, false, gT_M_W, updateT_M_W, status);
+            gMappingPipelineMono->mappingProcessRequest({image}, {pose}, /* fixedPose = */ false, gT_M_W, updateT_M_W, status);
             gT_M_W = updateT_M_W;
             switch (status) {
             case BOOTSTRAP:
